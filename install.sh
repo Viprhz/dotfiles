e41af065a1f1744d94f064c9ae3a4d4e8edb8b01
viprhz <viprhz@gmail.com>
2014-02-04 23:17:13 +0800

    update files

--- install.sh
+++ install.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 # 将dotfiles文件夹链接到home目录下，以后编辑配置文件会方便很多
+mv -P ~/dotfiles ~/Dropbox/Apps/
 ln -s ~/Dropbox/Apps/dotfiles ~/dotfiles
 
 # for vim
-# ln -s ~/dotfiles/vim ~/.vim
-# ln -s ~/dotfiles/vim/vimrc ~/.vimrc
-
-# for oh-my-zsh
-# ln -s ~/dotfiles/oh-my-zsh/zshrc ~/.zshrc
-
-# for config files
-# ln -s ~/dotfiles/config/gemrc ~/.gemrc
-
-ln -s ~/dotfiles/config/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/config/ssh ~/.ssh
+ln -s ~/dotfiles/config/.vimrc ~/.vimrc
+# for git
+ln -s ~/dotfiles/config/.gitconfig ~/.gitconfig
+# for zsh
+ln -s ~/dotfiles/config/.zshrc	~/.zshrc
